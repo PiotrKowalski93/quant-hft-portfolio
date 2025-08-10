ef90e8dd4b52e32c67ec1f95d2a9d44d4b8ba086
Piotr Kowalski <piotrkow93@gmail.com>
2025-08-10 17:00:14 +0200

    Simple strategy, comparison with buy and hold

--- Stock Data Analyzer/main.py
+++ Stock Data Analyzer/main.py
@@ -7,7 +7,7 @@ ticker = "AAPL"
 data = yf.download(ticker, start="2023-01-01", end="2025-01-01")
 
 if data.empty:
-    raise ValueError(f"Brak danych dla tickera: {ticker}")
+    raise ValueError(f"Ticker data is empty: {ticker}")
 
 # Test columns returned from Yahoo Finance
 #print(data.columns)
@@ -15,13 +15,14 @@ print(data.head(5))
 print()
 
 # Daily change 
-data['DReturn'] = data['Close'].pct_change()
+# .pct_change() Fractional change between the current and a prior element. 
+data['Daily Return'] = data['Close'].pct_change()
 
 # Drop first NaN row
 data = data.dropna()
-print(data[['Close', 'DReturn']].head())
+print(data[['Close', 'Daily Return']].head())
 
-# Moving avg for 20 and 50 days
+# Simple Moving Avrage for 20 and 50 days
 data['SMA20'] = data['Close'].rolling(window=20).mean()
 data['SMA50'] = data['Close'].rolling(window=50).mean()
 
@@ -30,10 +31,31 @@ data['SMA50'] = data['Close'].rolling(window=50).mean()
 #print(data['SMA50'])
 
 # Visualise data
+# plt.figure(figsize=(12,6))
+# plt.plot(data['Close'], label='Close')
+# plt.plot(data['SMA20'], label='SMA20')
+# plt.plot(data['SMA50'], label='SMA50')
+# plt.legend()
+# plt.title(f'{ticker} Price with SMA20 & SMA50')
+# plt.show()
+
+# Simple strategy
+# Buy when SMA20 > SMA50
+# Sell otherwise
+data['Position'] = 0
+data.loc[data['SMA20'] > data['SMA50'], 'Position'] = 1
+
+data['Strategy Return'] = data['Position'].shift(1) * data['Daily Return']
+
+print()
+#print(data[['Daily Return', 'Strategy Return']])
+
+# .cumprod() - Return cumulative product over a DataFrame or Series axis
+buy_hold_return = (1 + data['Daily Return']).cumprod()
+strategy_return = (1 + data['Strategy Return']).cumprod()
 plt.figure(figsize=(12,6))
-plt.plot(data['Close'], label='Close')
-plt.plot(data['SMA20'], label='SMA20')
-plt.plot(data['SMA50'], label='SMA50')
+plt.plot(buy_hold_return, label='Buy & Hold')
+plt.plot(strategy_return, label='SMA Strategy')
 plt.legend()
-plt.title(f'{ticker} Price with SMA20 & SMA50')
+plt.title(f'{ticker} Strategy vs Buy & Hold')
 plt.show()
