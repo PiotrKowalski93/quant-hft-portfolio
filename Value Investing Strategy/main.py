f96cc602ab6ab0e16c5bdfc405e104d865b9aa1b
Piotr Kowalski <piotrkow93@gmail.com>
2025-08-24 13:30:39 +0200

    Calculate Robust Value an Number of shares to buy

--- Value Investing Strategy/main.py
+++ Value Investing Strategy/main.py
@@ -14,10 +14,26 @@ def calculate_shares(portfolio_size, dataframe, column):
 
 ## Importing list of stocs
 stocks = pd.read_csv("./Equal-Weight S&P 500 Screener/sp_500_stocks.csv")
-data_columns = ['Ticker', 'Price', 'Trailing (P/E)', 'Forward (P/E)', 'Price to Book (P/B)', 'Price to Sales (P/S)', 'EV/EBITDA', 'EV/GP', 'Shares to Buy']
+data_columns = [
+    'Ticker', 
+    'Price', 
+    'Trailing (P/E)', 
+    'T P/E Per.',
+    'Forward (P/E)', 
+    'F P/E Per.',
+    'Price to Book (P/B)', 
+    'P/B Per.',
+    'Price to Sales (P/S)',
+    'P/S Per.',
+    'EV/EBITDA', 
+    'EV/EBITDA Per.',
+    'EV/GP', 
+    'EV/GP Per.',
+    'RV Score',
+    'Shares to Buy']
 
 lst = []
-for stock in stocks['Ticker'][:15]:
+for stock in stocks['Ticker'][:25]:
     ticker = yf.Ticker(stock)
     info = ticker.info
 
@@ -34,11 +50,18 @@ for stock in stocks['Ticker'][:15]:
         stock, 
         info.get("currentPrice"), 
         info.get("trailingPE"), 
+        'N/A',
         info.get("forwardPE"), 
+        'N/A',
         info.get("priceToBook"),
+        'N/A',
         info.get("priceToSalesTrailing12Months"),
+        'N/A',
         info.get("enterpriseToEbitda"),
+        'N/A',
         ev_gp,
+        'N/A',
+        'N/A',
         'N/A'])
 
 final_data = pd.DataFrame(lst, columns = data_columns)
@@ -47,6 +70,36 @@ final_data = final_data[final_data['Trailing (P/E)'] > 0]
 final_data.reset_index(inplace=True)
 final_data.drop('index', axis=1, inplace=True)
 
+print(final_data)
+
+# Calculate percentyles
+metrics = {
+    'Trailing (P/E)' : 'T P/E Per.',
+    'Forward (P/E)' : 'F P/E Per.',
+    'Price to Book (P/B)' : 'P/B Per.',
+    'Price to Sales (P/S)' : 'P/S Per.',
+    'EV/EBITDA' : 'EV/EBITDA Per.',
+    'EV/GP' : 'EV/GP Per.'
+    }
+
+for metric in metrics.keys():
+    for row in final_data.index:
+        final_data.loc[row, metrics[metric]] = stats.percentileofscore(final_data[metric], final_data.loc[row, metric])
+
+
+# Calculate Robust Value 
+for row in final_data.index:
+    sum = 0
+    for metric in metrics.keys():
+        sum += final_data.loc[row, metrics[metric]]
+    rv = sum/len(metrics)
+    final_data.loc[row, 'RV Score'] = rv
+
+final_data.sort_values('RV Score', ascending=False, inplace=True)
+final_data.reset_index(inplace=True)
+final_data.drop('index', axis=1, inplace=True)
+
+final_data = final_data[:10]
 calculate_shares(100000, final_data, 'Shares to Buy')
 
 print(final_data)
