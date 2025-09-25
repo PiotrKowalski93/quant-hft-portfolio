717850620cf92684fa0c1825fd16dd8ab9049c1d
Piotr Kowalski <piotrkow93@gmail.com>
2025-09-25 20:08:26 +0200

    Add processing market order

--- OrderBook/MatchingEngine/Trade.h
+++ OrderBook/MatchingEngine/Trade.h
@@ -10,3 +10,79 @@ struct Trade {
 	Ts timestamp;
 };
 
+class MatchingEngine {
+public: 
+	vector<Trade> process_marked_order_sell(Qty qty, Ts timestamp) {
+		vector<Trade> trades;
+		
+		// To prevent memory alloc resize
+		trades.reserve(10);
+
+		while (qty > 0 && !order_book.asks.empty())
+		{
+			auto best_ask_it = order_book.asks.begin();
+			auto& level_it = best_ask_it->second;
+
+			while (qty > 0 && !level_it.queue.empty())
+			{
+				auto& order = level_it.queue.front();
+				Qty trade_qty = min(qty, order.qty);
+
+				// Creates obj in vector mem
+				trades.emplace_back(Trade {
+					0,
+					order.id,
+					trade_qty,
+					order.price,
+					timestamp
+				});
+
+				order.qty -= trade_qty;
+				qty -= trade_qty;
+
+				// Remove order if fully consumed
+				if (order.qty == 0) level_it.queue.pop_front();
+
+				// Remove price level if empty
+				if (level_it.queue.empty()) order_book.asks.erase(best_ask_it);
+			}
+		}
+
+		return trades;
+	}
+
+	vector<Trade> process_marked_order_buy(Qty qty, Ts timestamp) {
+		vector<Trade> trades;
+		trades.reserve(10);
+
+		while (qty > 0 && !order_book.bids.empty()) {
+			auto best_bid_it = order_book.bids.begin();
+			auto& level_it = best_bid_it->second;
+
+			while (qty > 0 && !level_it.queue.empty()) {
+				auto order = level_it.queue.front();
+				Qty trade_qty = min(qty, order.qty);
+
+				trades.emplace_back(Trade{
+					order.id,
+					0,
+					trade_qty,
+					order.price,
+					timestamp
+					});
+
+				order.qty -= trade_qty;
+				qty -= trade_qty;
+
+				if (order.qty == 0) level_it.queue.pop_front();
+
+				if (level_it.queue.empty()) order_book.bids.erase(best_bid_it);
+			}
+		}
+
+		return trades;
+	}
+
+private:
+	OrderBook order_book;
+};
